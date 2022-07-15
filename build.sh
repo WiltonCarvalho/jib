65350387592e68ac6b79320a9647e0ebcade5ba9
WiltonCarvalho <45881665+WiltonCarvalho@users.noreply.github.com>
2022-07-15 19:25:58 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,29 +1,42 @@
 #!/bin/sh
 set -e
-if [ ! -f init.gradle ]; then
+
+# Gradle init file for Jib plugin outsite of build.gradle
 cat <<'EOF'> init.gradle
 initscript {
-    repositories {
-        maven {
-            url 'https://plugins.gradle.org/m2'
-        }
+  repositories {
+    maven {
+      url 'https://plugins.gradle.org/m2'
     }
-    dependencies {
-        classpath 'gradle.plugin.com.google.cloud.tools:jib-gradle-plugin:3.2.1'
-    }
+  }
+  dependencies {
+    classpath 'gradle.plugin.com.google.cloud.tools:jib-gradle-plugin:3.2.1'
+  }
 }
 rootProject {
-    afterEvaluate {
-        if (!project.plugins.hasPlugin('com.google.cloud.tools.jib')) {
-            project.apply plugin: com.google.cloud.tools.jib.gradle.JibPlugin
-            tasks.jib.dependsOn classes
-        }
+  afterEvaluate {
+    if (!project.plugins.hasPlugin('com.google.cloud.tools.jib')) {
+      project.apply plugin: com.google.cloud.tools.jib.gradle.JibPlugin
+      tasks.jib.dependsOn classes
     }
+  }
 }
 EOF
-fi
 
-podman run --platform=linux/amd64 --name builder -it --rm \
+# Extra directories to add to the image
+# Entrypoint script to allow custom commands
+mkdir -p extraDirectories/app
+cat <<'EOF'> extraDirectories/app/run.sh
+#!/bin/sh
+set -e
+CLASS_PATH="$(cat /app/jib-classpath-file)"
+MAIN_CLASS="$(cat /app/jib-main-class-file)"
+java -XshowSettings:vm -version
+exec java -cp $CLASS_PATH $MAIN_CLASS
+EOF
+
+# Build with Podman
+podman run --platform=linux/amd64 -it --rm \
   -v /tmp:/tmp \
   -e GRADLE_USER_HOME=/tmp/build_cache/gradle \
   -v $PWD:/code:ro -w /code \
@@ -32,21 +45,26 @@ podman run --platform=linux/amd64 --name builder -it --rm \
       set -ex
       cp -r /code /home/code
       cd /home/code
-      pwd
       exec ./gradlew --info --init-script init.gradle jibBuildTar \
-        -Djib.console=plain \
+        -Djib.container.user=999:0 \
+        -Djib.container.workingDirectory=/app \
+        -Djib.container.entrypoint=/app/run.sh \
+        -Djib.container.args= \
+        -Djib.extraDirectories.paths=$PWD/extraDirectories \
+        -Djib.extraDirectories.permissions=/app/run.sh=755,/app=775 \
         -Djib.from.image=docker.io/library/openjdk:11-jre \
         -Djib.from.platforms=linux/amd64 \
         -Djib.allowInsecureRegistries=true \
         -Djib.outputPaths.tar=/tmp/build_cache/jib-image.tar
     '
 
-printf '\nLoading Image...'
+# Load the Jib image on Podman
+printf '\nLoading Image...\n'
 podman load -i /tmp/build_cache/jib-image.tar | \
   awk '{print $NF}' | \
   xargs -i podman tag {} test
 
-printf '\nLoaded Image...'
+printf '\nLoaded Image...\n'
 podman image tree test
 
-printf '\nRun "podman run -it --rm -p 8080:8080 test"'
+printf '\nRun "podman run -it --rm -p 8080:8080 test"\n\n'
