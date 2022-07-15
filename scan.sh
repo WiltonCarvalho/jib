3541ab19285e8c4722787af030a7bf38d00195e1
WiltonCarvalho <45881665+WiltonCarvalho@users.noreply.github.com>
2022-07-15 16:52:20 -0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -3,9 +3,10 @@ set -ex
 podman run --platform=linux/amd64 --name scan -it --rm \
   -v /tmp:/tmp \
   -e TRIVY_CACHE_DIR=/tmp/build_cache/trivy \
+  --entrypoint= \
   docker.io/aquasec/trivy \
-    image --ignore-unfixed --severity HIGH,CRITICAL \
-    --exit-code 0 \
-    --security-checks vuln \
-    --vuln-type os,library \
-    --input /tmp/build_cache/jib-image.tar
+    trivy image --ignore-unfixed --severity HIGH,CRITICAL \
+      --exit-code 0 \
+      --security-checks vuln \
+      --vuln-type os,library \
+      --input /tmp/build_cache/jib-image.tar
